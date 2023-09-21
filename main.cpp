5dc263261947632c67a823e0004a47f737728220
Omar Zaidi <31871102+OmarZaidi@users.noreply.github.com>
2023-09-20 21:38:32 -1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,16 +28,23 @@ return sum;
 
 int fibonacci(int num){
     
-    int sum;
-    int i = 1;
-    
-    while(i < num){
-        
-        
-        
+    int t1 = 0, t2 = 1, nextTerm = 0, n;
+
+    cout << "Enter a positive number: ";
+    cin >> n;
+
+    // displays the first two terms which is always 0 and 1
+    cout << "Fibonacci Series: " << t1 << ", " << t2 << ", ";
+
+    nextTerm = t1 + t2;
+
+    while(nextTerm <= n) {
+        cout << nextTerm << ", ";
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
     }
-    
-    
+    return 0;  
 }
 
 
